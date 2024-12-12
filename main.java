4ed2961ec4aa6a3df259424223c097be9a77f813
Ethan Robert <ethan.rbrt90@gmail.com>
2024-12-12 12:31:23 +0100

    Additions

--- main.java
+++ main.java
@@ -1,5 +1,15 @@
+import extensions.File;
+
 class Fraquizz extends Program {
 
-    void
+    void algorithm () {
+
+        Parser parser = new Parser();
+        parser.filepath = "config/regions.csv";
+        parser.file = loadCSV(parser.filepath);
+        parser.rows = rowCount(parser.file);
+
+        
+    }
 
 }
